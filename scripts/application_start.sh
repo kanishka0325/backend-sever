b54eec5ed8553780d6e35726a5c0e088206c885c
Eshan Gallage <eshan@web3genes.com>
2023-05-30 12:28:07 +0530

    application_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,6 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/backend-sever/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/backend-sever/deploy.log
-sudo pm2 restart nodejs-express-app >> /home/ec2-user/backend-sever/deploy.log
+echo 'pm2 restart backend-sever' >> /home/ec2-user/backend-sever/deploy.log
+sudo pm2 restart backend-sever >> /home/ec2-user/backend-sever/deploy.log
 sudo pm2 save >> /home/ec2-user/backend-sever/deploy.log
