1032b8746a1ecc36e6869880856c6c50706d1636
kanishka0325 <purna@web3genes.com>
2023-05-30 10:23:51 +0530

    refactor

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({msg: 'Hello, Backend CI/CD Pipeline!'});
+});
+
+app.get('/test', (req, res)=>{
+    res.json({msg: 'Working with CI/CD Push'});
+});
+
+app.get('/my', (req, res)=>{
+    res.json({msg: 'My Works '});
+});
+
+app.listen(4000);
