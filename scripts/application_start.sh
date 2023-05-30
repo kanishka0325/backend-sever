2cd113de1746092e77281c3141d57b08d7478970
Kanishka@Web3 Genes <133314889+kanishka0325@users.noreply.github.com>
2023-05-30 11:40:07 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,3 +4,4 @@ echo 'run application_start.sh: ' >> /home/ec2-user/backend-sever/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/backend-sever/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/backend-sever/deploy.log
+sudo pm2 save >> /home/ec2-user/backend-sever/deploy.log
